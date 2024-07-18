7a902be34ebfa30e09c21063f6fbc84ea7b9b4f5
unknown <ssimuni>
2024-07-18 19:28:16 +0600

    attachment related tasks done

--- src/pages/student/fee/logos.jsx
+++ src/pages/student/fee/logos.jsx
@@ -12,24 +12,24 @@ const Logos = ({degree}) => {
                 <div className="lg:flex md:flex w-96 mx-auto">
                 <Link to={`/certificate-withdrawal-form/${degreeName}`}>
                     <div className="mx-auto my-5 border-2 border-black rounded-lg p-2 w-28 h-20 flex justify-center items-center">
-                        <img src="./bkash-logo.png" alt="" />
+                        <img src="/bkash-logo.png" alt="" />
                     </div>
                     </Link>
                     
                     <div className="mx-auto my-5 border-2 border-black rounded-lg p-2 w-28 h-20 flex justify-center items-center">
-                        <Link to="/certificate-withdrawal-form"><img src="./public/nagad.png" alt="" /></Link>
+                        <Link to="/certificate-withdrawal-form"><img src="/nagad.png" alt="" /></Link>
                     </div>
                     <div className="mx-auto my-5 border-2 border-black rounded-lg p-2 w-28 h-20 flex justify-center items-center">
-                        <Link to={`/certificate-withdrawal-form/${degreeName}`}><img src="rocket.png" alt="" /></Link>
+                        <Link to={`/certificate-withdrawal-form/${degreeName}`}><img src="/rocket.png" alt="" /></Link>
                     </div>
                 </div>
 
                 <div className="lg:flex md:flex w-72 mx-auto">
                     <div className="mx-auto my-5 border-2 border-black rounded-lg p-2 w-28 h-20 flex justify-center items-center">
-                        <Link to={`/certificate-withdrawal-form/${degreeName}`}> <img src="visa.png" alt="" /></Link>
+                        <Link to={`/certificate-withdrawal-form/${degreeName}`}> <img src="/visa.png" alt="" /></Link>
                     </div>
                     <div className="mx-auto my-5 border-2 border-black rounded-lg p-2 w-28 h-20 flex justify-center items-center">
-                        <Link to={`/certificate-withdrawal-form/${degreeName}`}> <img src="upay.png" alt="" /></Link>
+                        <Link to={`/certificate-withdrawal-form/${degreeName}`}> <img src="/upay.png" alt="" /></Link>
                     </div>
                 </div>
 
