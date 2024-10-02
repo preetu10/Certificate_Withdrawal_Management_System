ad2dc2358875832b9ef13c28d104d8f691b38746
unknown <ssimuni>
2024-10-02 19:26:49 +0600

    navbar responsive done

--- src/customHooks/useAxiosPublic.jsx
+++ src/customHooks/useAxiosPublic.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-// export const baseURL = "http://bike-csecu.com:5000/api";
+//export const baseURL = "http://bike-csecu.com:5000/api";
 export const baseURL = "http://localhost:5000/api";
 const axiosPublic = axios.create({
   baseURL: baseURL,
